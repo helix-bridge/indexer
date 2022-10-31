af4d851dc4744f39f0f80050b2b346760f3aa568
xiaoch05 <xiaoch2010@gmail.com>
2022-10-31 16:43:59 +0800

    repair s2s dispatch result

--- subql/pangolin-parachain/src/handlers/event.ts
+++ subql/pangolin-parachain/src/handlers/event.ts
@@ -66,7 +66,7 @@ export class EventHandler {
   }
 
   public async handleBridgeDispatchEvent() {
-    const [_, [laneId, nonce]] = JSON.parse(this.data) as [string, [string, bigint]];
+    const [_, [laneId, nonce], result] = JSON.parse(this.data);
     const event = new BridgeDispatchEvent(this.s2sEventId(laneId, nonce));
 
     event.index = this.index;
@@ -74,6 +74,9 @@ export class EventHandler {
     event.data = this.data;
     event.block = this.simpleBlock();
     event.timestamp = this.timestamp;
+    if (this.method === 'MessageDispatched' && result.ok === undefined) {
+        event.method = 'MessageDispatched(Err)'
+    }
 
     await event.save();
   }
