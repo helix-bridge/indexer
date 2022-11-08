dd19b8acb03b68810b441dccc357bdfef0758f01
xiaoch05 <xiaoch2010@gmail.com>
2022-11-08 14:36:00 +0800

    init

---
+++ subgraph/solo2para/backing/src/mapping.ts
@@ -0,0 +1,48 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  TokenLocked,
+  TokenUnlockedForFailed,
+  RemoteIssuingFailure,
+} from "../generated/NativeParachainBacking/NativeParachainBacking"
+import { TransferRecord, RefundTransferRecord } from "../generated/schema"
+
+export function handleTokenLocked(event: TokenLocked): void {
+  let message_id = event.params.nonce.toHexString();
+  let entity = TransferRecord.load(message_id);
+  if (entity == null) {
+      entity = new TransferRecord(message_id);
+  }
+  entity.sender = event.params.sender;
+  entity.receiver = event.params.recipient;
+  entity.amount = event.params.amount;
+  entity.transaction_hash = event.transaction.hash;
+  entity.start_timestamp = event.block.timestamp;
+  entity.fee = event.params.fee;
+  entity.save();
+}
+
+export function handleTokenUnlockedForFailed(event: TokenUnlockedForFailed): void {
+  let id = event.params.nonce.toHexString();
+  let entity = TransferRecord.load(id);
+  if (entity == null) {
+      return;
+  }
+  entity.withdraw_amount = event.params.amount;
+  entity.withdraw_transaction = event.transaction.hash;
+  entity.withdraw_timestamp = event.block.timestamp;
+  entity.save();
+}
+
+// refund txs
+export function handleRemoteIssuingFailure(event: RemoteIssuingFailure): void {
+  let id = event.params.refundNonce.toHexString();
+  let entity = RefundTransferRecord.load(id);
+  if (entity == null) {
+      entity = new RefundTransferRecord(id);
+  }
+  entity.source_id = event.params.failureNonce.toHexString();
+  entity.timestamp = event.block.timestamp;
+  entity.transaction_hash = event.transaction.hash;
+  entity.save();
+}
+
