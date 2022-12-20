658b9df18c29e369b87f3d71575f0042b5dda976
xiaoch05 <xiaoch2010@gmail.com>
2022-12-20 09:49:10 +0800

    init

--- apollo/src/app.module.ts
+++ apollo/src/app.module.ts
@@ -9,16 +9,13 @@ import { AccountModule } from './account/account.module';
 import { AggregationModule } from './aggregation/aggregation.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Substrate2dvmModule } from './substrate2dvm/substrate2dvm.module';
-import { Substrate2parachainModule } from './substrate2parachain/substrate2parachain.module';
 import { TasksModule } from './tasks/tasks.module';
-import { CbridgeModule } from './cbridge/cbridge.module';
-import { S2sv2Module } from './s2sv2/s2sv2.module';
+//import { CbridgeModule } from './cbridge/cbridge.module';
+//import { S2sv2Module } from './s2sv2/s2sv2.module';
+//import { S2sv21Module } from './s2sv21/s2sv21.module';
 import { StatisticModule } from './statistic/statistic.module';
 import { XcmModule } from './xcm/xcm.module';
 import { Sub2ethv2Module } from './sub2ethv2/sub2ethv2.module';
-import { WtokenModule } from './wtoken/wtoken.module';
-import { S2sv21Module } from './s2sv21/s2sv21.module';
 
 const chainEnvFilePath = `.env.${process.env.NODE_ENV || 'prod'}`;
 
@@ -43,15 +40,12 @@ export class BigIntScalar extends BigInt {}
     ScheduleModule.forRoot(),
     TasksModule,
     AggregationModule,
-    Substrate2dvmModule,
-    Substrate2parachainModule,
-    CbridgeModule,
-    S2sv2Module,
     StatisticModule,
     XcmModule,
     Sub2ethv2Module,
-    WtokenModule,
-    S2sv21Module,
+    //CbridgeModule,
+    //S2sv2Module,
+    //S2sv21Module,
   ],
   controllers: [AppController],
   providers: [AppService, BigIntScalar],
--- apollo/src/s2sv21/transfer.service.ts
+++ apollo/src/s2sv21/transfer.service.ts
@@ -27,6 +27,11 @@ export class TransferService extends BaseTransferServiceT3 {
           to: 'xWCRAB',
           address: '0x2d2b97ea380b0185e9fdf8271d1afb5d2bf18329',
         },
+        //{
+          //from: 'xWRING',
+          //to: 'WRING',
+          //address: '0x273131f7cb50ac002bdd08ca721988731f7e1092',
+        //},
       ],
     },
     {
@@ -47,6 +52,11 @@ export class TransferService extends BaseTransferServiceT3 {
           to: 'WCRAB',
           address: '0x656567eb75b765fc320783cc6edd86bd854b2305',
         },
+        //{
+          //from: 'WRING',
+          //to: 'xWRING',
+          //address: '0xe7578598aac020abfb918f33a20fad5b71d670b4',
+        //},
       ],
     },
   ];
