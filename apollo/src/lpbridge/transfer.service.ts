08e0a4a72119aaf4002bbdd5b5a7454c678a4c7e
xiaoch05 <xiaoch2010@gmail.com>
2023-02-13 11:01:03 +0800

    add ethereum chain

--- apollo/src/lpbridge/transfer.service.ts
+++ apollo/src/lpbridge/transfer.service.ts
@@ -7,9 +7,11 @@ import { AddressTokenMap } from '../base/AddressToken';
 export class TransferService extends BaseTransferServiceT2 {
   private readonly crabSub2SubLpEndpoint = this.configService.get<string>('CRAB_S2S_LP_ENDPOINT');
   private readonly darwiniaSub2SubLpEndpoint = this.configService.get<string>('DARWINIA_S2S_LP_ENDPOINT');
-  //private readonly ethereumEndpoint = this.configService.get<string>('ETHEREUM_ENDPOINT');
+  private readonly ethereumSub2EthEndpoint = this.configService.get<string>('ETHEREUM_S2E_LP_ENDPOINT');
+  private readonly darwiniaSub2EthLpEndpoint = this.configService.get<string>('DARWINIA_S2E_LP_ENDPOINT');
 
   formalChainTransfers: PartnerT2[] = [
+    /*
     {
       chainId: 44,
       chain: 'crab-dvm',
@@ -22,11 +24,19 @@ export class TransferService extends BaseTransferServiceT2 {
       url: this.darwiniaSub2SubLpEndpoint,
       bridge: 'sub2sublp',
     },
-    //{
-    //chainId: 1,
-    //chain: 'ethereum',
-    //url: this.ethereumEndpoint,
-    //},
+    */
+    {
+      chainId: 1,
+      chain: 'ethereum',
+      url: this.ethereumSub2EthEndpoint,
+      bridge: 'sub2ethlp',
+    },
+    {
+      chainId: 46,
+      chain: 'darwinia-dvm',
+      url: this.darwiniaSub2EthLpEndpoint,
+      bridge: 'sub2ethlp',
+    },
   ];
 
   testChainTransfers: PartnerT2[] = [];
@@ -52,6 +62,13 @@ export class TransferService extends BaseTransferServiceT2 {
       },
       '0xe7578598aac020abfb918f33a20fad5b71d670b4': {
         token: 'WRING',
+        decimals: 1e18,
+        origin: 'WRING',
+      },
+    },
+    'ethereum': {
+      '0x9469d013805bffb7d3debe5e7839237e535ec483': {
+        token: 'RING',
         decimals: 1e18,
         origin: 'WRING',
       },
