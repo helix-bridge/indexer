8f81f6bb1e39162182437f01a5901c98f0965215
xiaoch05 <xiaoch2010@gmail.com>
2023-11-16 23:35:55 +0800

    bugfix lnbridge relayer info & add crab<>darwinia

--- apollo/src/lnbridgev20/transfer.service.ts
+++ apollo/src/lnbridgev20/transfer.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { BaseTransferServiceT2, PartnerT2 } from '../base/TransferServiceT2';
+import { BaseTransferServiceT3, PartnerT3 } from '../base/TransferServiceT3';
 import { AddressTokenMap } from '../base/AddressToken';
 
 @Injectable()
-export class TransferService extends BaseTransferServiceT2 {
+export class TransferService extends BaseTransferServiceT3 {
   private readonly lnEthereumDefaultEndpoint = this.configService.get<string>(
     'LN_ETHEREUM_DEFAULT_ENDPOINT'
   );
@@ -44,143 +44,338 @@ export class TransferService extends BaseTransferServiceT2 {
   private readonly lnDarwiniaDefaultEndpoint = this.configService.get<string>(
     'LN_DARWINIA_DEFAULT_ENDPOINT'
   );
+  private readonly lnCrabDefaultEndpoint = this.configService.get<string>(
+    'LN_CRAB_DEFAULT_ENDPOINT'
+  );
 
-  formalChainTransfers: PartnerT2[] = [
-    {
-      chainId: 1,
-      chain: 'ethereum',
-      url: this.lnEthereumOppositeEndpoint,
-      bridge: 'opposite',
-      symbols: [
-        {
-          symbol: 'RING',
-          address: '0x9469D013805bFfB7D3DEBe5E7839237e535ec483',
-          protocolFee: 100000000000000000000,
-          decimals: 18,
-        },
-      ],
-    },
-    {
-      chainId: 42161,
-      chain: 'arbitrum',
-      url: this.lnArbitrumOppositeEndpoint,
-      bridge: 'opposite',
-      symbols: [
-        {
-          symbol: 'RING',
-          address: '0x9e523234D36973f9e38642886197D023C88e307e',
-          protocolFee: 100000000000000000000,
-          decimals: 18,
-        },
-      ],
-    },
-    {
-      chainId: 1,
-      chain: 'ethereum',
-      url: this.lnEthereumDefaultEndpoint,
-      bridge: 'default',
-      symbols: [
-        {
-          symbol: 'RING',
-          address: '0x9469D013805bFfB7D3DEBe5E7839237e535ec483',
-          protocolFee: 100000000000000000000,
-          decimals: 18,
-        },
-      ],
-    },
-    {
-      chainId: 42161,
-      chain: 'arbitrum',
-      url: this.lnArbitrumDefaultEndpoint,
-      bridge: 'default',
-      symbols: [
-        {
-          symbol: 'RING',
-          address: '0x9e523234D36973f9e38642886197D023C88e307e',
-          protocolFee: 100000000000000000000,
-          decimals: 18,
-        },
-        {
-          symbol: 'USDT',
-          address: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
-          protocolFee: 100000,
-          decimals: 6,
-        },
-      ],
-    },
-    {
-      chainId: 5000,
-      chain: 'mantle',
-      url: this.lnMantleDefaultEndpoint,
-      bridge: 'default',
-      symbols: [
-        {
-          symbol: 'USDT',
-          address: '0x201eba5cc46d216ce6dc03f6a759e8e766e956ae',
-          protocolFee: 100000,
-          decimals: 6,
-        },
-      ],
-    },
-    {
-      chainId: 137,
-      chain: 'polygon',
-      url: this.lnPolygonDefaultEndpoint,
-      bridge: 'default',
-      symbols: [
-        {
-          symbol: 'RING',
-          address: '0x9c1c23e60b72bc88a043bf64afdb16a02540ae8f',
-          protocolFee: 100000000000000000000,
-          decimals: 18,
-        },
-      ],
-    },
-    {
-      chainId: 324,
-      chain: 'zksync',
-      url: this.lnZkSyncDefaultEndpoint,
-      bridge: 'default',
-      symbols: [
-        {
-          symbol: 'USDT',
-          address: '0x493257fd37edb34451f62edf8d2a0c418852ba4c',
-          protocolFee: 100000,
-          decimals: 6,
-        },
-      ],
-    },
-    {
-      chainId: 534352,
-      chain: 'scroll',
-      url: this.lnScrollDefaultEndpoint,
-      bridge: 'default',
-      symbols: [
-        {
-          symbol: 'USDT',
-          address: '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df',
-          protocolFee: 100000,
-          decimals: 6,
-        },
-      ],
-    },
-    {
-      chainId: 46,
-      chain: 'darwinia-dvm',
-      url: this.lnDarwiniaDefaultEndpoint,
-      bridge: 'default',
-      symbols: [
-        {
-          symbol: 'RING',
-          address: '0x0000000000000000000000000000000000000000',
-          protocolFee: 100000000000000000000,
-          decimals: 18,
-        },
-      ],
-    },
+  formalChainTransfers: PartnerT3[] = [
+      {
+          chainId: 1,
+          chainName: 'ethereum',
+          defaultEndpoint: this.lnEthereumDefaultEndpoint,
+          oppositeEndpoint: this.lnEthereumOppositeEndpoint,
+          tokens: [
+              {
+                  fromSymbol: 'RING',
+                  fromAddress: '0x9469D013805bFfB7D3DEBe5E7839237e535ec483',
+                  decimals: 18,
+                  remoteInfos: [
+                      {
+                          toChain: 42161,
+                          toSymbol: 'RING',
+                          toAddress: '0x9e523234D36973f9e38642886197D023C88e307e',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'arbitrum-l2',
+                      },
+                  ]
+              },
+          ]
+      },
+      {
+          chainId: 42161,
+          chainName: 'arbitrum',
+          defaultEndpoint: this.lnArbitrumDefaultEndpoint,
+          oppositeEndpoint: this.lnArbitrumOppositeEndpoint,
+          tokens: [
+              {
+                  fromSymbol: 'RING',
+                  fromAddress: '0x9e523234D36973f9e38642886197D023C88e307e',
+                  decimals: 18,
+                  remoteInfos: [
+                      {
+                          toChain: 1,
+                          toSymbol: 'RING',
+                          toAddress: '0x9469D013805bFfB7D3DEBe5E7839237e535ec483',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'opposite',
+                          channel: 'arbitrum-l2',
+                      },
+                      {
+                          toChain: 137,
+                          toSymbol: 'RING',
+                          toAddress: '0x9c1c23e60b72bc88a043bf64afdb16a02540ae8f',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                      {
+                          toChain: 46,
+                          toSymbol: 'RING',
+                          toAddress: '0x0000000000000000000000000000000000000000',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'msgline',
+                      }
+                  ]
+              },
+              {
+                  fromSymbol: 'USDT',
+                  fromAddress: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
+                  decimals: 6,
+                  remoteInfos: [
+                      {
+                          toChain: 5000,
+                          toSymbol: 'USDT',
+                          toAddress: '0x201eba5cc46d216ce6dc03f6a759e8e766e956ae',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                      {
+                          toChain: 324,
+                          toSymbol: 'USDT',
+                          toAddress: '0x493257fd37edb34451f62edf8d2a0c418852ba4c',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      }
+                  ]
+              },
+          ]
+      },
+      {
+          chainId: 5000,
+          chainName: 'mantle',
+          defaultEndpoint: this.lnMantleDefaultEndpoint,
+          oppositeEndpoint: null,
+          tokens: [
+              {
+                  fromSymbol: 'USDT',
+                  fromAddress: '0x201eba5cc46d216ce6dc03f6a759e8e766e956ae',
+                  decimals: 6,
+                  remoteInfos: [
+                      {
+                          toChain: 42161,
+                          toSymbol: 'USDT',
+                          toAddress: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                      {
+                          toChain: 324,
+                          toSymbol: 'USDT',
+                          toAddress: '0x493257fd37edb34451f62edf8d2a0c418852ba4c',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                      {
+                          toChain: 534352,
+                          toSymbol: 'USDT',
+                          toAddress: '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      }
+                  ]
+              }
+          ]
+      },
+      {
+          chainId: 137,
+          chainName: 'polygon',
+          defaultEndpoint: this.lnPolygonDefaultEndpoint,
+          oppositeEndpoint: null,
+          tokens: [
+              {
+                  fromSymbol: 'RING',
+                  fromAddress: '0x9c1c23e60b72bc88a043bf64afdb16a02540ae8f',
+                  decimals: 18,
+                  remoteInfos: [
+                      {
+                          toChain: 42161,
+                          toSymbol: 'RING',
+                          toAddress: '0x9e523234D36973f9e38642886197D023C88e307e',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      }
+                  ]
+              }
+          ]
+      },
+      {
+          chainId: 324,
+          chainName: 'zksync',
+          defaultEndpoint: this.lnZkSyncDefaultEndpoint,
+          oppositeEndpoint: null,
+          tokens: [
+              {
+                  fromSymbol: 'USDT',
+                  fromAddress: '0x493257fd37edb34451f62edf8d2a0c418852ba4c',
+                  decimals: 6,
+                  remoteInfos: [
+                      {
+                          toChain: 42161,
+                          toSymbol: 'USDT',
+                          toAddress: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                      {
+                          toChain: 5000,
+                          toSymbol: 'USDT',
+                          toAddress: '0x201eba5cc46d216ce6dc03f6a759e8e766e956ae',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                      {
+                          toChain: 534352,
+                          toSymbol: 'USDT',
+                          toAddress: '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                  ]
+              }
+          ]
+      },
+      {
+          chainId: 534352,
+          chainName: 'scroll',
+          defaultEndpoint: this.lnScrollDefaultEndpoint,
+          oppositeEndpoint: null,
+          tokens: [
+              {
+                  fromSymbol: 'USDT',
+                  fromAddress: '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df',
+                  decimals: 6,
+                  remoteInfos: [
+                      {
+                          toChain: 5000,
+                          toSymbol: 'USDT',
+                          toAddress: '0x201eba5cc46d216ce6dc03f6a759e8e766e956ae',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      },
+                      {
+                          toChain: 324,
+                          toSymbol: 'USDT',
+                          toAddress: '0x493257fd37edb34451f62edf8d2a0c418852ba4c',
+                          protocolFee: 100000,
+                          decimals: 6,
+                          bridgeType: 'default',
+                          channel: 'layerzero',
+                      }
+                  ]
+              }
+          ]
+      },
+      {
+          chainId: 46,
+          chainName: 'darwinia-dvm',
+          defaultEndpoint: this.lnDarwiniaDefaultEndpoint,
+          oppositeEndpoint: null,
+          tokens: [
+              {
+                  fromSymbol: 'RING',
+                  fromAddress: '0x0000000000000000000000000000000000000000',
+                  decimals: 18,
+                  remoteInfos: [
+                      {
+                          toChain: 42161,
+                          toSymbol: 'RING',
+                          toAddress: '0x9e523234D36973f9e38642886197D023C88e307e',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'msgline',
+                      },
+                      {
+                          toChain: 44,
+                          toSymbol: 'xWRING',
+                          toAddress: '0x273131F7CB50ac002BDd08cA721988731F7e1092',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'sub2sub',
+                      }
+                  ]
+              },
+              {
+                  fromSymbol: 'xWCRAB',
+                  fromAddress: '0x656567Eb75b765FC320783cc6EDd86bD854b2305',
+                  decimals: 18,
+                  remoteInfos: [
+                      {
+                          toChain: 44,
+                          toSymbol: 'CRAB',
+                          toAddress: '0x0000000000000000000000000000000000000000',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'sub2sub',
+                      }
+                  ]
+              }
+          ]
+      },
+      {
+          chainId: 44,
+          chainName: 'crab-dvm',
+          defaultEndpoint: this.lnCrabDefaultEndpoint,
+          oppositeEndpoint: null,
+          tokens: [
+              {
+                  fromSymbol: 'CRAB',
+                  fromAddress: '0x0000000000000000000000000000000000000000',
+                  decimals: 18,
+                  remoteInfos: [
+                      {
+                          toChain: 46,
+                          toSymbol: 'xWCRAB',
+                          toAddress: '0x656567Eb75b765FC320783cc6EDd86bD854b2305',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'sub2sub',
+                      }
+                  ]
+              },
+              {
+                  fromSymbol: 'xWRING',
+                  fromAddress: '0x273131F7CB50ac002BDd08cA721988731F7e1092',
+                  decimals: 18,
+                  remoteInfos: [
+                      {
+                          toChain: 46,
+                          toSymbol: 'RING',
+                          toAddress: '0x0000000000000000000000000000000000000000',
+                          protocolFee: 100000000000000000000,
+                          decimals: 18,
+                          bridgeType: 'default',
+                          channel: 'sub2sub',
+                      }
+                  ]
+              }
+          ]
+      }
   ];
+  
+  testChainTransfers: PartnerT3[] = [];
 
-  testChainTransfers: PartnerT2[] = [
+  /*
+  testChainTransfers: PartnerT3[] = [
     {
       chainId: 5,
       chain: 'goerli',
@@ -442,10 +637,10 @@ export class TransferService extends BaseTransferServiceT2 {
       ],
     },
   ];
-
-  addressToTokenInfo: { [key: string]: AddressTokenMap } = {};
+  */
 
   // message channel is used to withdraw liquidity
+  /*
   public readonly messageChannel = {
     goerli: {
       'arbitrum-goerli': 'arbitrum-l2',
@@ -510,8 +705,13 @@ export class TransferService extends BaseTransferServiceT2 {
     },
     'darwinia-dvm': {
       arbitrum: 'msgline',
+      'crab-dvm': 'sub2sub',
     },
+    'crab-dvm': {
+      'darwinia-dvm': 'sub2sub',
+    }
   };
+  */
   readonly isTest = this.configService.get<string>('CHAIN_TYPE') === 'test';
 
   constructor(public configService: ConfigService) {
