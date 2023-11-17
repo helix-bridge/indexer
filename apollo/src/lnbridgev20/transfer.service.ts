36678245a7ab8638519fff20a97e334ab63e734d
xiaoch05 <xiaoch2010@gmail.com>
2023-11-17 14:59:29 +0800

    rmv unused

--- apollo/src/lnbridgev20/transfer.service.ts
+++ apollo/src/lnbridgev20/transfer.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { BaseTransferServiceT3, PartnerT3 } from '../base/TransferServiceT3';
-import { AddressTokenMap } from '../base/AddressToken';
 
 @Injectable()
 export class TransferService extends BaseTransferServiceT3 {
