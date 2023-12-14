eb2a00336ad214c2d91020bbdda9fedebab2f7ad
xiaoch05 <xiaoch2010@gmail.com>
2023-12-14 09:16:07 +0800

    add xtoken module

--- apollo/src/app.module.ts
+++ apollo/src/app.module.ts
@@ -17,6 +17,7 @@ import { S2sv21Module } from './s2sv21/s2sv21.module';
 import { LnbridgeModule } from './lnbridge/lnbridge.module';
 import { L2arbitrumModule } from './l2arbitrum/l2arbitrum.module';
 import { Lnbridgev20Module } from './lnbridgev20/lnbridgev20.module';
+import { xTokenModule } from './xtoken/xtoken.module';
 
 const chainEnvFilePath = `.env.${process.env.NODE_ENV || 'prod'}`;
 
@@ -48,6 +49,7 @@ export class BigIntScalar extends BigInt {}
     S2sv21Module,
     L2arbitrumModule,
     Lnbridgev20Module,
+    xTokenModule,
   ],
   controllers: [AppController],
   providers: [AppService, BigIntScalar],
